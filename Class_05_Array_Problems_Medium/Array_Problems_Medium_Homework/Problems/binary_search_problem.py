55082ec953c30cc855f5fed1fccf28a03816de7b
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-01-18 16:13:38 -0700

    binary search

--- Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/Problems/binary_search_problem.py
+++ Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/Problems/binary_search_problem.py
@@ -5,3 +5,22 @@
 input_array = [1, 2, 5, 9, 12, 15, 21, 28, 99, 100, 117]
 input_target = 5
 # Output = 2
+
+def binary_search(array, n):
+    l = 0
+    r = len(array) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        if array[mid] == n:
+            return mid
+        
+        if array[mid] < n:
+            l = mid + 1
+        elif array[mid] > n:
+            r = mid - 1
+        
+    return -1
+
+print(binary_search(input_array, input_target))
