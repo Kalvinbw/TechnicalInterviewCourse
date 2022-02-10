ee3c4aafddee06602b60f480a9838c6b60c01473
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-02-09 18:15:09 -0700

    linked List work

--- Class_10_Linked_List_Problems_Easy/ClassPractice/Problems/python_linked_list_problem.py
+++ Class_10_Linked_List_Problems_Easy/ClassPractice/Problems/python_linked_list_problem.py
@@ -1,4 +1,7 @@
 # Create a linked list using python
+from os import curdir
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -17,6 +20,42 @@ class LinkedList:
             prev = current
             current = next
         self.head = prev
+    
+    def remove_dups(self):
+        prev = self.head
+        current = prev.next
+
+        while current:
+            if prev.data == current.data:
+                prev.next = current.next
+                current = current.next
+            else:
+                prev = current
+                current = current.next
+    
+    def remove_dups_unsorted(self):
+        di = {self.head.data: self.head}
+
+        current = self.head.next
+        prev = self.head
+
+        while current:
+            if current.data in di:
+                prev.next = current.next                
+            else:
+                di[current.data] = current
+                prev = current
+            current = current.next
+
+    def remove_skipped_nodes(self, m, n):
+        current = self.head
+        for _ in range(m-1):
+            current = current.next
+
+        
+        for _ in range(n):
+            current.next = current.next.next
+
 
     def append(self, data):
         if self.head is None:
@@ -63,21 +102,42 @@ class LinkedList:
         print(current.data)
         
 
+def recursive_reverse(node):
+    if node is None:
+        return node
+    if node.next is None:
+        return node
+    
+    tempnode = recursive_reverse(node.next)
+    node.next.next = node
+    node.next = None
+    return tempnode
+
 
 
 llist = LinkedList()
 llist.append(1)
+llist.append(2)
+llist.append(3)
 llist.append(4)
 llist.append(5)
 llist.append(6)
-llist.prepend(-3)
+llist.append(7)
+llist.append(8)
+# llist.prepend(1)
 llist.displayList()
 print()
 
-llist.reverse()
+# llist.remove_dups_unsorted()
+# llist.reverse()
+
+llist.remove_skipped_nodes(2, 1)
 llist.displayList()
 print()
 
-llist.removeNode(6)
-llist.removeNode(4)
-llist.displayList()
+# llist.removeNode(6)
+# llist.removeNode(4)
+# llist.displayList()
+
+
+
