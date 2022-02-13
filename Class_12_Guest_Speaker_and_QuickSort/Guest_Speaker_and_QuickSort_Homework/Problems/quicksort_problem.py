48cfffc732df5b39b944ef403d5a0c67a41fff19
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-02-12 20:20:13 -0700

    quicksort

--- Class_12_Guest_Speaker_and_QuickSort/Guest_Speaker_and_QuickSort_Homework/Problems/quicksort_problem.py
+++ Class_12_Guest_Speaker_and_QuickSort/Guest_Speaker_and_QuickSort_Homework/Problems/quicksort_problem.py
@@ -1,1 +1,20 @@
 # Write a quicksort
+def quick_sort(sequence):
+    length = len(sequence)
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+    
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+    
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+print(quick_sort([5,4,3,7,89,2,1,0,7,65,43,23,29]))
